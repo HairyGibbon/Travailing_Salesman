88a4eca29f3932014f75dc84d1ca71e574f98f16
HairyGibbon <72943461+HairyGibbon@users.noreply.github.com>
2020-11-03 21:27:27 +0000

    Added functionality to make more like a game

--- graph.py
+++ graph.py
@@ -6,17 +6,49 @@ class Graph:
 
     __coordinates = []
     __connections = []
+    __pressed_num = [0]
+    __pressed_coo = []
+    __route_length = 0
 
+    def init(self):
+        self.__coordinates = []
+        self.__connections = []
+        self.__pressed_num = [0]
+        self.__pressed_coo = []
+        self.__route_length = 0
+
+    # Setters
     def set_coordinates(self, *coordinates):
-        for co in coordinates:
-            self.__coordinates.append(co)
+        for coo in coordinates:
+            self.__coordinates.append(coo)
 
     def set_connections(self, *connections):
-        for co in connections:
-            self.__connections.append(co)
+        for coo in connections:
+            self.__connections.append(coo)
 
+    def set_pressed_num(self, node):
+        self.__pressed_num.append(node)
+        num1 = self.__pressed_num[-1]
+        num2 = self.__pressed_num[-2]
+        for con in self.__connections:
+            if (con[0] == num1 and con[1] == num2) or (con[0] == num2 and con[1] == num1):
+                self.__route_length += con[2]
+
+    def set_pressed_coo(self, node):
+        self.__pressed_coo.append(node)
+
+    # Getters
     def get_coordinates(self):
         return self.__coordinates
 
     def get_connections(self):
         return self.__connections
+
+    def get_pressed_num(self):
+        return self.__pressed_num
+
+    def get_pressed_coo(self):
+        return self.__pressed_coo
+
+    def get_route_length(self):
+        return self.__route_length
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 """TODO:
-Add background image of London
-Make it so the player can select the path
-Make the guy go along the lines
-Add High score system
-Add Sam's google travelling salesman algorithm
-Add pause menu with credits
+Pause menu
+Credits
+Player Movement
 """
 
 import pygame
 import graph
 import time
+import random
 
 # Initialise
 pygame.init()
@@ -17,7 +15,7 @@ pygame.init()
 # Create Screen
 screen_width = 800
 screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))  # pixel size
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Clock
 clock = pygame.time.Clock()
@@ -26,12 +24,43 @@ clock = pygame.time.Clock()
 background = pygame.image.load("Map.png")
 
 # Title and Icon
-pygame.display.set_caption("Epic Graph Game")
+pygame.display.set_caption("Graph Game")
 icon = pygame.image.load("big_ben.png")
 pygame.display.set_icon(icon)
 
 # Player Icon
+player_width = 32
+player_height = 32
 player_image = pygame.image.load("person-walking.png")
+player_image = pygame.transform.scale(player_image, (32, 32))
+
+# Some Colours
+black = (0, 0, 0)
+green = (54, 186, 145)
+d_green = (36, 124, 97)
+b_green = (72, 248, 193)
+blue = (0, 0, 150)
+d_blue = (0, 0, 100)
+b_blue = (0, 0, 200)
+red = (150, 0, 0)
+d_red = (100, 0, 0)
+b_red = (200, 0, 0)
+
+# Some Fonts
+small_font = pygame.font.Font("freesansbold.ttf", 20)
+large_font = pygame.font.Font("freesansbold.ttf", 100)
+
+# Generating our Graph
+A = graph.Graph()
+
+# for num in range(random.randint(4, 10)):
+#     A.set_coordinates((random.randint(50, 550), random.randint(50, 550)))
+#     for p in range(len(A.get_coordinates()) - 1):
+#         if bool(random.getrandbits(1)):
+#             A.set_connections((p, num, random.randint(1, 10)))
+#
+# # A.set_coordinates((500, 500), (100, 100), (100, 200), (200, 100), (200, 200))
+# # A.set_connections((0, 1, 1), (0, 2, 4), (0, 3, 2), (1, 2, 2), (1, 3, 3), (2, 3, 8))
 
 
 # Player Function
@@ -39,16 +68,77 @@ def player(x, y):
     screen.blit(player_image, (x, y))
 
 
+# Initialising the Title
+title_surf = large_font.render("Graph Game", True, black)
+title_rect = title_surf.get_rect()
+title_rect.center = ((screen_width / 2), (screen_height / 2))
+
+
+# Rectangular Button
+def button_oblong(rect, text, dark_col, bright_col, command):
+
+    button_surf = small_font.render(text, True, black)
+    button_rect = button_surf.get_rect()
+    button_rect.center = (rect[0] + rect[2] / 2, rect[1] + rect[3] / 2)
+
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed(3)
+
+    if rect[0] < mouse[0] < rect[0] + rect[2] and rect[1] < mouse[1] < rect[1] + rect[3]:
+        pygame.draw.rect(screen, bright_col, rect)
+        if click[0] == 1:
+            command()
+    else:
+        pygame.draw.rect(screen, dark_col, rect)
+
+    screen.blit(button_surf, button_rect)
+
+
+# Circle button
+def node_circle(centre, place, radius, dark_col, mid_col, bright_col):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed(3)
+
+    if (centre[0] - mouse[0])**2 + (centre[1] - mouse[1])**2 < radius**2:
+        pygame.draw.circle(screen, bright_col, centre, radius)
+        if click[0] == 1:
+            pygame.draw.circle(screen, dark_col, centre, radius)
+            if A.get_pressed_num()[-1] != place and test_connection(A.get_pressed_num()[-1], place):
+                A.set_pressed_num(place)
+                A.set_pressed_coo(centre)
+    else:
+        pygame.draw.circle(screen, mid_col, centre, radius)
+
+
 # Menu Screen
 def game_menu():
-    pass
+    while True:
+        for event in pygame.event.get():
+
+            # Exiting
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        screen.fill(green)
+
+        # Display Title
+        screen.blit(title_surf, title_rect)
+
+        # Display Working Buttons
+        button_oblong((200, 450, 100, 50), "Start", blue, b_blue, game_loop)
+        button_oblong((500, 450, 100, 50), "Credits", blue, b_blue, credit)
+
+        pygame.display.update()
+
+        clock.tick(60)  # 60 fps
 
 
 # Fail Screen
+def message(text):
 
-def fail_message(text):
-    large_font = pygame.font.Font("freesansbold.ttf", 100)
-    text_surf = large_font.render(text, True, (0, 0, 0))
+    # Set up the Fail Message
+    text_surf = large_font.render(text, True, black)
     text_rect = text_surf.get_rect()
     text_rect.center = ((screen_width / 2), (screen_height / 2))
 
@@ -56,21 +146,46 @@ def fail_message(text):
 
     pygame.display.update()
 
-    time.sleep(2)
+    time.sleep(2)  # Can't do anything for two seconds
+
+    A.init()
+
+    game_loop()
 
 
-# Generating our Graph
-A = graph.Graph()
-
-A.set_coordinates((100, 100), (100, 200), (200, 100), (200, 200))
-A.set_connections((0, 1, 1), (1, 2, 2), (2, 0, 4), (3, 2, 8))
-print(A.get_coordinates())
+# Credit Screen
+def credit():
+    pass
 
 
+def score():
+    score_surf = small_font.render("Distance: " + str(A.get_route_length()), True, black)
+    score_rect = score_surf.get_rect()
+    score_rect.topright = (800, 0)
+
+    screen.blit(score_surf, score_rect)
+
+
+def test_connection(p1, p2):
+    for con in A.get_connections():
+        if (con[0] == p1 and con[1] == p2) or (con[0] == p2 and con[1] == p1):
+            return True
+    return False
+
+
+# Main Game Loop
 def game_loop():
 
-    player_x = 370
-    player_y = 480
+    # Generating Graph
+    for num in range(random.randint(4, 10)):
+        A.set_coordinates((random.randint(50, 550), random.randint(50, 550)))
+        for p in range(len(A.get_coordinates()) - 1):
+            if bool(random.getrandbits(1)):
+                A.set_connections((p, num, random.randint(1, 10)))
+
+    # Initialising the player position
+    player_x = A.get_coordinates()[0][0] - player_width / 2
+    player_y = A.get_coordinates()[0][1] - player_height - 10
     player_x_change = 0
     player_y_change = 0
 
@@ -83,55 +198,41 @@ def game_loop():
                 pygame.quit()
                 quit()
 
-            # Keystrokes
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player_x_change = -5
-                if event.key == pygame.K_RIGHT:
-                    player_x_change = 5
-                if event.key == pygame.K_UP:
-                    player_y_change = -5
-                if event.key == pygame.K_DOWN:
-                    player_y_change = 5
-                # Fail Message
-                if event.key == pygame.K_f:
-                    fail_message("Fail")
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    player_x_change = 0
-                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    player_y_change = 0
+        if len(set(A.get_pressed_num())) == len(A.get_coordinates()):
+            message("Win")
 
         # Player Movement
         player_x += player_x_change
         player_y += player_y_change
+        if A.get_pressed_coo():
+            player_x = A.get_pressed_coo()[-1][0] - player_width / 2
+            player_y = A.get_pressed_coo()[-1][1] - player_height - 10
 
-        if player_x <= 0:
-            player_x = 0
-        elif player_x >= 736:
-            player_x = 736
-
-        if player_y <= 0:
-            player_y = 0
-        elif player_y >= 536:
-            player_y = 536
-
-        # Background
-        screen.fill((54, 186, 145))  # rgb colour
+        # Draw Background
+        screen.fill(green)  # rgb colour
         screen.blit(background, (0, 0))
 
         # Draw Lines
         for con in A.get_connections():
-            pygame.draw.line(screen, (150, 0, 0), A.get_coordinates()[con[0]], A.get_coordinates()[con[1]], con[2])
+            pygame.draw.line(screen, red, A.get_coordinates()[con[0]], A.get_coordinates()[con[1]], con[2])
 
         # Draw Circles
         for coo in A.get_coordinates():
-            pygame.draw.circle(screen, (0, 0, 150), coo, 10)
+            place = A.get_coordinates().index(coo)
+            if A.get_pressed_num().count(place) == 0:
+                node_circle(coo, place, 10, d_blue, blue, b_blue)
+            else:
+                node_circle(coo, place, 10, d_green, green, b_green)
 
+        # Draw Figure
         player(player_x, player_y)
+
+        # Draw Score
+        score()
+
         pygame.display.update()
 
-        clock.tick(60)
+        clock.tick(60)  # 60 fps
 
 
-game_loop()
+game_menu()
